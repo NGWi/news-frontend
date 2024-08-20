1919ad48e94a49b66d1fef46ae40910e30d7dac1
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 16:56:06 -0400

    Changed to wait for submit button b/c of rate-limiting.

--- src/ArticlesPage.jsx
+++ src/ArticlesPage.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ArticlesIndex } from "./ArticlesIndex";
 
 export function ArticlesPage() {
