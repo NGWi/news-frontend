98bc5ef0d76ee4a643ba3cecf0a4edafea7fa458
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 16:14:19 -0400

    Added search.

--- src/ArticlesPage.jsx
+++ src/ArticlesPage.jsx
@@ -3,23 +3,27 @@ import { useEffect, useState } from "react";
 import { ArticlesIndex } from "./ArticlesIndex";
 
 export function ArticlesPage() {
+  const [searchTerms, setSearchTerms] = useState("API");
   const [articles, setArticles] = useState([]);
 
   const handleIndex = () => {
     console.log("handleIndex");
-    axios.get("http://localhost:3000/articles.json").then((response) => {
+    console.log("searchTerms:", searchTerms);
+    axios.get("http://localhost:3000/articles.json?search_terms=" + searchTerms + "").then((response) => {
       console.log(response.data);
       setArticles(response.data);
     });
   };
 
-  useEffect(handleIndex, []);
+  useEffect(handleIndex);
 
   return (
     <main>
       <div>
-        <ArticlesIndex articles={articles} />
+        Live Search:
+        <input value={searchTerms} onChange={(e) => setSearchTerms(e.target.value)} type="text" />
       </div>
+        <ArticlesIndex articles={articles} />
     </main>
   );
 }
