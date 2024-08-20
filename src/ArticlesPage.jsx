f1eddb459a61f249a74283235ec636d8c26543c1
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 16:54:22 -0400

    Changed to wait for submit button b/c of rate-limiting.

--- src/ArticlesPage.jsx
+++ src/ArticlesPage.jsx
@@ -15,13 +15,18 @@ export function ArticlesPage() {
     });
   };
 
-  useEffect(handleIndex);
+// useEffect(handleIndex); // Doing Search button instead. Live reload Will get {"status"=>"error",
+  //  "code"=>"rateLimited",
+  //  "message"=>
+  //   "You have made too many requests recently. Developer accounts are limited to 100 requests over a 24 hour period (50 requests available every 12 hours). Please upgrade to a paid plan if you need more requests."}
+
 
   return (
     <main>
       <div>
         Live Search:
         <input value={searchTerms} onChange={(e) => setSearchTerms(e.target.value)} type="text" />
+        <button onClick={handleIndex}>Search</button>
       </div>
         <ArticlesIndex articles={articles} />
     </main>
